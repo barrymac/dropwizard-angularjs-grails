a613551036286e7cdfa004dab107b41961851d5e
barry <barrymac@gmail.com>
2013-05-29 01:58:05 +0100

    angular saving people

--- grails-app/dropwizard/com/example/helloworld/resources/PeopleCreateDropwizardResource.groovy
+++ grails-app/dropwizard/com/example/helloworld/resources/PeopleCreateDropwizardResource.groovy
@@ -2,27 +2,24 @@ package com.example.helloworld.resources
 
 import com.example.helloworld.core.Person
 import com.example.helloworld.core.PersonDTO
+import com.yammer.metrics.annotation.Timed
 
-import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 
-@Path('/people')
+@Path('/people/save')
 @Produces(MediaType.APPLICATION_JSON)
-class PeopleDropwizardResource {
+class PeopleCreateDropwizardResource {
 
     def personService
 
     @POST
+    @Timed(name = 'post-requests-people')
     PersonDTO createPerson(PersonDTO dto) {
         Person person = personService.create(dto)
         new PersonDTO(person)
     }
 
-    @GET
-    List<PersonDTO> listPeopleDw() {
-        Person.list().collect { new PersonDTO(it) }
-    }
 }
--- grails-app/dropwizard/com/example/helloworld/resources/PeopleListDropwizardResource.groovy
+++ grails-app/dropwizard/com/example/helloworld/resources/PeopleListDropwizardResource.groovy
@@ -5,6 +5,7 @@ import com.example.helloworld.core.PersonDTO
 import com.yammer.metrics.annotation.Timed
 
 import javax.ws.rs.GET
+import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
@@ -12,6 +13,12 @@ import javax.ws.rs.core.MediaType
 @Path('/people/list')
 @Produces(MediaType.APPLICATION_JSON)
 class PeopleListDropwizardResource {
+
+    @POST
+    PersonDTO createPerson(PersonDTO dto) {
+        Person person = personService.create(dto)
+        new PersonDTO(person)
+    }
 
     @GET
     @Timed(name = 'get-requests-people')
